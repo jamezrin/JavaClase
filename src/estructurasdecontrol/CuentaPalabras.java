15675770422a2c5a147c5921c8e21e0bc5ce867d
minauz <35262298+minauz@users.noreply.github.com>
2018-09-28 16:33:16 +0200

    Update CuentaPalabras.java

--- src/estructurasdecontrol/CuentaPalabras.java
+++ src/estructurasdecontrol/CuentaPalabras.java
@@ -1,14 +1,35 @@
-package estructurasdecontrol;
 
-import java.util.Scanner;
+/*Realizar un pseudocódigo que visualice en pantalla 
+ * el número de palabras que tiene una frase acabada en punto.*/
+
+package ejerciciosCasa;
+
+import java.io.IOException;
 
 public class CuentaPalabras {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		// Declaración de variables.
+		int contadorP = 0;
+		char caracter;
+		boolean validadorP = true;
+
+		System.out.println("Introduzca una frase: ");
+		contadorP++;
+		do {
+
+			caracter = (char) System.in.read();
+			if (caracter == ' ') {
+				contadorP++;
+			} else if (caracter == '.') {
+				validadorP = false;
+			}
+
+		} while (validadorP == true);
+
+		System.out.println("Usted a introducido: " + contadorP + " palabras. ");
 
 	}
-	Scanner entrada = new  Scanner(System.in);
-	
-	
 
 }
